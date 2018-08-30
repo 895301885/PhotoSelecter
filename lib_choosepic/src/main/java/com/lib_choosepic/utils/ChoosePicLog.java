e2436f870b8d34771de9d7a97b766367f94359bf
lishiguo <lishiguo@4399inc.com>
2018-08-30 16:33:29 +0800

    gailsd

--- lib_choosepic/src/main/java/com/lib_choosepic/utils/ChoosePicLog.java
+++ lib_choosepic/src/main/java/com/lib_choosepic/utils/ChoosePicLog.java
@@ -13,7 +13,7 @@ public class ChoosePicLog {
             Log.i(LOG_TAG, logMsg);
         }
     }
-
+  int ss=5;
     public static void e(String logMsg) {
         if (SHOW_LOG) {
             Log.e(LOG_TAG, logMsg);
